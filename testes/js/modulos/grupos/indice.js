b3298f9088b04745782689ef7c32a0063cc11943
Leo Felippe <leofelippedias@hotmail.com.br>
2016-09-21 17:53:52 -0300

    adicionando eventos as rotas dos modulos

--- testes/js/modulos/grupos/indice.js
+++ testes/js/modulos/grupos/indice.js
@@ -5,9 +5,11 @@
 define([
   "aplicativo"
 , "modulos/baseDasRotas"
+, "modulos/grupos/roteador/rotas"
 ], function (
   aplic
 , Base
+, Rotas
 ) {
   'use strict';
  
@@ -15,57 +17,5 @@ define([
 
   Registrar('BAIXO', 'Iniciando o modulo de grupos.');
 
-  var Rotas = {
-
-    visaoDoPainel: null,
-    visaoDoTopoPainel: null,
-    visaoDosGruposPainel: null,
-
-    iniciar: function() {
-      _.bindAll(this, 'suporte');
-
-      Registrar('BAIXO', 'Adicionando rotas dos grupos.');
-
-      aplic.adcRota('GrupoUm', this.suporte);
-      aplic.adcRota('GrupoDois', this.suporte);
-      aplic.adcRota('GrupoTres', this.suporte);
-    },
-
-    suporte: function(rota, id) {
-
-      Registrar('BAIXO', 'Acessando suporte da rota '+ rota);
-
-      var painel = this.visaoDoPainel = this.reusarVisao("VisaoBaseDePainel", function() { });
-      var topoDoPainel = this.visaoDoTopoPainel = this.reusarVisao("VisaoBaseDeTopoPainel", function() { });
-      var grupos = this.visaoDosGruposPainel = this.reusarVisao("VisaoGruposDoPainel", function() { });
-
-      // Esconde todos os grupos do conteudo do painel
-      painel.esconderTodosOsGrupos();
-
-      // Esconde todos os conteudos de todos os grupos.
-      grupos.esconderTodosOsConteudosDosGrupos();
-
-      // não apresenta qualquer aviso anteriormente apresentada
-      painel.esconderTodosAvisos();
-
-      if (rota === 'GrupoUm') { 
-        var grupoUm = this.reusarVisao("VisaoDoGrupoUm", function() { });
-
-        grupoUm.esconderTodosAvisos();
-        painel.mostrarUmGrupo('div.grupo-um');
-        topoDoPainel.selecionarItemMenu('ul.menu-painel-topo li.item-grupo-um');
-      } else if (rota === 'GrupoDois') {
-        painel.mostrarUmGrupo('div.grupo-dois');
-        topoDoPainel.selecionarItemMenu('ul.menu-painel-topo li.item-grupo-dois');
-      } else if (rota === 'GrupoTres') {
-        painel.mostrarUmGrupo('div.grupo-tres');
-      } 
-    }
-  }
-
-  var Uniao = {};
-  _.extend(Uniao, Base);
-  _.extend(Uniao, Rotas);
-
-  Uniao.iniciar();
+  Rotas.iniciar();
 });
---
+++ testes/js/modulos/grupos/roteador/rotas.js
@@ -0,0 +1,71 @@
+define([
+  "aplicativo"
+, "modulos/baseDasRotas"
+], function (
+  aplic
+, Base
+) {
+  'use strict';
+
+  var Registrar = _.bind(aplic.registrar, { envolucro: 'modulos/grupos/roteador/rotas' });
+
+  var Rotas = {
+
+    visaoDoPainel: null,
+    visaoDoTopoPainel: null,
+    visaoDosGruposPainel: null,
+
+    iniciar: function() {
+      _.bindAll(this, 'suporte');
+
+      Registrar('BAIXO', 'Adicionando rotas dos grupos.');
+
+      aplic.adcRota('GrupoUm', this.suporte);
+      aplic.adcRota('GrupoDois', this.suporte);
+      aplic.adcRota('GrupoTres', this.suporte);
+    },
+
+    suporte: function(rota, id) {
+
+      Registrar('BAIXO', 'Acessando suporte da rota '+ rota);
+
+      // Esconde todos os conteudos de todos os grupos.
+      aplic.evts.trigger('grupos-conteudos:esconder');
+
+      // Esconde todos os grupos do conteudo do painel
+      aplic.evts.trigger('painel-grupos:esconder');
+
+      // não apresenta qualquer aviso anteriormente apresentada no painel.
+      aplic.evts.trigger('painel-avisos:esconder');
+
+      if (rota === 'GrupoUm') { 
+
+        // esconde avisos do grupo um
+        aplic.evts.trigger('grupo-um-avisos:esconder');
+
+        // mostra o grupo um do painel.
+        aplic.evts.trigger('painel-grupo:mostrar', 'div.grupo-um');
+        
+        // selecionamos um item do menu de navegação do topo.
+        aplic.evts.trigger('item-navegacao-topo:selecionar', 'ul.menu-painel-topo li.item-grupo-um'); 
+      } else if (rota === 'GrupoDois') {
+
+        // mostra o grupo dois do painel.
+        aplic.evts.trigger('painel-grupo:mostrar', 'div.grupo-dois');
+
+        // selecionamos um item do menu de navegação do topo.
+        aplic.evts.trigger('item-navegacao-topo:selecionar', 'ul.menu-painel-topo li.item-grupo-dois');
+      } else if (rota === 'GrupoTres') {
+
+        // mostra o grupo três do painel.
+        aplic.evts.trigger('painel-grupo:mostrar', 'div.grupo-tres');
+      } 
+    }
+  }
+
+  var Uniao = {};
+  _.extend(Uniao, Base);
+  _.extend(Uniao, Rotas);
+
+  Uniao.iniciar();
+});
